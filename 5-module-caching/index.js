235baa546e995cbdb131b7f937936731eca614ba
Mehul Kundu <programmer.mehul@gmail.com>
2024-01-29 01:33:07 +0530

    1st day

---
+++ 5-module-caching/index.js
@@ -0,0 +1,8 @@
+const superHero = require("./suoerhero");
+console.log(superHero.getName());
+
+superHero.setName("Superman");
+console.log(superHero.getName());
+
+const newSuperHero = require("./suoerhero");
+console.log(newSuperHero.getName());
